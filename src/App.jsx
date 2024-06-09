cfda3a2a1ccbf32c4cb7a0fe60103e136827b429
SnappyDragon64 <pg61151@gmail.com>
2024-06-09 06:43:58 +0530

    WIP app prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import MaxWidthWrapper from "./components/MaxWidthWrapper.jsx";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        // TODO: Separate into components
+        <div className="bg-theme-color-dark min-h-screen flex flex-col">
+            </* Header */ div className="sticky top-0 w-full bg-theme-color-light">
+                <MaxWidthWrapper>
+                    <div className="flex p-2.5">
+                        <div className="grow flex justify-start">
+                            </* Logo */ p className="text-2xl font-bold text-white">Meshi.</p>
+                        </div>
+                        <div className="grow flex justify-end">
+                            </* AniList Username */ div></div>
+                        </div>
+                    </div>
+                </MaxWidthWrapper>
+            </div>
+            </* Main */ MaxWidthWrapper>
+                <div className="flex flex-col p-8">
+                </div>
+            </MaxWidthWrapper>
+        </div>
+    )
 }
