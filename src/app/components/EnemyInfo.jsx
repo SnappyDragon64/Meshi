f8170dfe7c4dbaa8ed9e00dff306fd7a7232d2c2
SnappyDragon64 <pg61151@gmail.com>
2024-06-22 06:29:07 +0530

    Update EnemyInfo.jsx

--- src/app/components/EnemyInfo.jsx
+++ src/app/components/EnemyInfo.jsx
@@ -1,14 +1,18 @@
 const EnemyInfo = ({name, hp, weaknesses, resistances, image}) => {
   return (
     <div className="w-full grid rounded-lg overflow-hidden">
-      <div className="col-start-1 row-start-1 flex flex-row-reverse z-0">
+      <div className="col-start-1 row-start-1 z-0">
+        <div
+          className="bg-theme-color-primary w-full h-full"/>
+      </div>
+      <div className="col-start-1 row-start-1 flex flex-row-reverse z-10">
         <img src={image} className="object-cover w-1/2"/>
       </div>
-      <div className="col-start-1 row-start-1 z-10">
+      <div className="col-start-1 row-start-1 z-20">
         <div
-          className="bg-gradient-to-t from-theme-color-tertiary via-theme-color-dark to-theme-color-dark opacity-80 w-full h-full"/>
+            className="bg-gradient-to-t from-theme-color-tertiary via-theme-color-tertiary via-20% to-transparent opacity-80 w-full h-full"/>
       </div>
-      <div className="col-start-1 row-start-1 z-20 flex">
+      <div className="col-start-1 row-start-1 z-30 flex">
         <div className="bg-theme-color-highlight w-2 h-full"/>
         <div className="p-4">
           <p className="text-xl font-bold text-theme-text-color">{name}</p>
